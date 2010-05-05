96f2767ded56721d56b996627e3ee3ddc3fdf96f
Leroy Campbell <leroy@artisonian.com>
2010-05-05 18:55:48 -0400

    Step 3: added database initialization function to app

--- flaskr.py
+++ flaskr.py
@@ -1,4 +1,6 @@
 # all the imports
+from __future__ import with_statement # for python 2.5
+from contextlib import closing
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
     abort, render_template, flash
@@ -17,6 +19,12 @@ app.debug = DEBUG
 
 def connect_db():
     return sqlite3.connect(DATABASE)
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 
 if __name__ == '__main__':
