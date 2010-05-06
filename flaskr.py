18224e3aa98930bf17c940b931f4d936e0d22961
Leroy Campbell <leroy@artisonian.com>
2010-05-05 22:47:00 -0400

    Step 5: Added view functions

--- flaskr.py
+++ flaskr.py
@@ -35,6 +35,42 @@ def after_request(response):
     g.db.close()
     return response
 
+@app.route('/')
+def show_entries():
+    cur = g.db.execute('SELECT title, text FROM entries ORDER BY id DESC')
+    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    return render_template('show_entries.html', entries=entries)
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('INSERT INTO entries (title, text) VALUES (?, ?)',
+        [request.form['title'], request.form['text']])
+    g.db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != USERNAME:
+            error = 'Invalid username'
+        elif request.form['password'] != PASSWORD:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You have been logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You have been logged out')
+    return redirect(url_for('show_entries'))
+
 
 if __name__ == '__main__':
     app.run()
